f0dfd03e3e93bd22d09f1847be08419e9a4122b0
abulfayes <afayes@hotmail.co.uk>
2017-11-30 02:55:52 +0000

    delayed spring boot app start so that couchbase is ready otherwise an excpetion is thrown

--- src/main/java/com/form3/payment/Application.java
+++ src/main/java/com/form3/payment/Application.java
@@ -1,14 +1,42 @@
 package com.form3.payment;
 
+import static springfox.documentation.builders.PathSelectors.ant;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class Application {
 
-	public static final String VERSION = "/api/payment/v1" ;
+	public static final String VERSION = "/api/v1" ;
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
+		// wait for couchbase to be ready
+		// todo - a better way is to do a health check via a couchbase rest endpoint
+		Thread.sleep(20000);
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public static Docket submissionApi() {
+		ApiInfo apiInfo = new ApiInfoBuilder()
+				.title("Payment API")
+				.description("Payment API")
+				.build();
+
+		return new Docket(DocumentationType.SWAGGER_2)
+				.apiInfo(apiInfo)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("com.form3.payment"))
+				.paths(ant("/**/*"))
+				.build();
 	}
 }
